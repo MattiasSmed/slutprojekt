9a480ddc620ab71bbb0edf98365726ce9cfec8ce
root <mangegreiff@gmail.com>
2016-04-29 09:53:04 +0200

    More updates

--- PHP/abselseth.php
+++ PHP/abselseth.php
@@ -15,8 +15,83 @@
 include('../TEMPLATE/header.php');
 ?>
 
-<h1>Mr Olaus Jørgensen Abelseth</h1>
+<!-- TODO: Add image. Position the image next to picture -->
 
+<section class="common_section">
+    <article>
+        <h1>Mr Olaus Jørgensen Abelseth</h1>
+    </article>
+
+    <article>
+        <p>
+            Mr Olaus Jørgensen Abelseth ("Ole") was born on 10 June 1886, the son of Jergon Abelseth and Anna Hatlevik,
+            at a farm in Ørskog - east of Ålesund a small fishing village in Norway.
+        </p>
+        <p>
+            Olaus had worked as a sailor and casual labourer. He went to America with his brother Hans in 1902 or 1903
+            and lived in Hatton, North Dakota where he worked on different farms in the Red River Valley.
+        </p>
+        <p>In 1908 he established a livestock farm in Perkins County, South Dakota. After a difficult period at his farm
+            Olaus decided to visit his relatives in Norway and he departed for Glasgow by steamship from New York in the
+            late Autumn of 1911. From Glasgow he continued his journey to Scandinavia.</p>
+        <p>In April 1912 Olaus began the return journey, his stated destination was Johan B. Abelseth 1112 Lincoln St.
+            Minneapolis. Travelling with him were five other Norwegians: Adolf Humblen, Anna Salkjelsvik, Peter Søholt
+            (a cousin), Sigurd Hansen Moen (married to Olaus' sister Inge) and Karen Marie Abelseth. Karen Abelseth was
+            not a relative (? a cousin) but was the daughter of one of Olaus' neighbours when he lived in Norway. They
+            new each other well, so, since Karen was only 16, her father asked Olaus if he could look after her on the
+            trip to America.</p>
+        <p>The party set sail from Ålesund to Newcastle via Bergen and boarded the Titanic at Southampton. Olaus and
+            Humblen shared a cabin toward the bow on F-Deck (G-63) from where, on the night of the disaster, he made his
+            way aft along the working alleyway 'Scotland Road' on E deck to meet Karen. He finally found her near the
+            main third class staircase towards the stern and then she, Olaus and the rest of their group made their way
+            to the aft well deck.</p>
+        <p>They waited on the poop deck for instructions. At about 1:30 third class women were finally allowed onto the
+            Boat Deck, followed by the men at 2:00. While many decided to remain on the poop Olaus and his relatives
+            made for the Boat Deck. Olous together with Moen and Søholt placed Karen Abelseth into a boat.</p>
+        <p>They waited on the poop deck for instructions. At about 1:30 third class women were finally allowed onto the
+            Boat Deck, followed by the men at 2:00. While many decided to remain on the poop Olaus and his relatives
+            made for the Boat Deck. Olous together with Moen and Søholt placed Karen Abelseth into a boat.</p>
+        <p>With the last boat pulling away they heard a call for sailors, some of the crew were trying to free a
+            collapsible and Olaus who had six years of sailing experience as a fisherman was tempted to assist but his
+            cousin and brother in law urged him to stay with them.</p>
+        <p>"I was standing there, and I asked my brother-in-law if he could swim and he said no. I asked my cousin if he
+            could swim and he said no. So we could see the water coming up, the bow of the ship was going down, and
+            there was kind of an explosion. We could hear the popping and cracking, and the deck raised up and got so
+            steep that the people could not stand on their feet on the deck. So they fell down and slid on the deck into
+            the water right on the ship."</p>
+        <p>When all the boats had gone Olaus and his relatives found themselves near the fourth funnel, as the Titanic
+            sank deeper they clung to the falls of a lifeboat davit. His brother in law urged him to jump for it but
+            Olaus waited. When the water was only five feet away they plunged in. As he surfaced Olaus became entangled
+            in a line but somehow managed to break free, when he looked around him his brother in law and cousin were
+            nowhere to be seen, they had been washed away.</p>
+        <p>Olaus swam for twenty minutes in the icy water before finally reaching collapsible A. surrounded by dead and
+            dying he tried to pull himself into the waterlogged boat but someone inside shouted 'don't capsize the
+            boat', so Olaus clung to the side for a while before eventually dragging himself aboard.</p>
+        <p>As they rowed through the night the survivors in Collapsible A prayed, and, although nearly waist deep in
+            water Olaus tried to revive a fellow passenger who lay freezing in the bottom of the boat, he lifted him up
+            and discovered that it was a man from New Jersey with whom Olaus had shared a carriage on the boat train to
+            Southampton 1. When the Carpathia was sighted he urged the man to look up, but as dawn broke the man slipped
+            away. Another man put his arms around Abelseth to relieve cramps caused by the cold but eventually he too
+            died and Olaus had to prise the man's arm off him.</p>
+        <p>When he finally reached the deck of the Carpathia at 7:00 am he was given a warm blanket he then headed for
+            the dining room for some brandy and a hot coffee. With cabin space at a premium Olaus found he had to sleep
+            on deck and lay down to sleep in the same clothes that he had worn all night in the flooded boat.</p>
+        <p>In New York he stayed a few days at St. Vincent Hospital. He also testified before the US senate Inquiry
+            before moving on to Minneapolis. During 1912 and 1913 he travelled in Canada, Indianapolis and Montana
+            before returning to his farm in South Dakota.</p>
+        <p>In July 1915 he married Anna Grinde in South Dakota. Anna was Ole's first wife, he her second husband. Anna
+            had been born in Grinde Norway 6 October 1877. Her father had died at sea in Norwegian waters near
+            Sognefjorden on 19 June 1886. The death occurred only nine day after Olaus was born, Anna was eight.</p>
+        <p>Olaus worked his farm for a further 30 years and he and Anna had four children; their second son died at the
+            age of 3½, the other children were: George, Helen and Mae.</p>
+        <p>He retired in 1946 to Reeder, North Dakota. In 1948 they moved to Tacoma Washington and in 1960 to Whetting,
+            North Dakota before settling in Hettinger, Adams Co., North Dakota.</p>
+        <p>Anna celebrated her 100th birthday in 1977; she died in August 1978. Olaus died on 4 December 1980. His
+            daughter Mae (Mrs Jim Omodt) lived in Sandpoint, Idaho2 and his son George in Prairie City, South
+            Dakota.</p>
+    </article>
+
+</section>
 <?php
 include('../TEMPLATE/footer.php');
 ?>
